381db1d387ab99eea2efbe12ef32507d52c02340
Morukutsu <ulderic.k@free.fr>
2015-11-03 00:48:03 +0100

    [bugfix] RCTViewManager view method must always return a new instance

--- ios/RCTCameraManager.m
+++ ios/RCTCameraManager.m
@@ -16,11 +16,7 @@ RCT_EXPORT_MODULE();
 
 - (UIView *)view
 {
-    if(!self.camera){
-        self.camera = [[RCTCamera alloc] initWithManager:self bridge:self.bridge];
-        return self.camera;
-    }
-    return self.camera;
+    return [[RCTCamera alloc] initWithManager:self bridge:self.bridge];
 }
 
 RCT_EXPORT_VIEW_PROPERTY(aspect, NSInteger);
@@ -309,7 +305,6 @@ RCT_EXPORT_METHOD(toggleCamera:(BOOL)state) {
 #endif
 
   dispatch_async(self.sessionQueue, ^{
-    self.camera = nil;
     [self.previewLayer removeFromSuperlayer];
     [self.session stopRunning];
     for(AVCaptureInput *input in self.session.inputs) {
